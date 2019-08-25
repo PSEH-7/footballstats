71e83b8f2989767d26d3e7ed6e9138df0906fd41
PSEH-7 <Hiring@WKWIN3163907.global.publicisgroupe.net>
2019-08-25 15:56:42 +0530

    Intial Commit with project setup

---
+++ src/main/java/com/sapient/footballstats/FootballstatsApplication.java
@@ -0,0 +1,13 @@
+package com.sapient.footballstats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FootballstatsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FootballstatsApplication.class, args);
+	}
+
+}
